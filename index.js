4fcf09931089126cc11e51b1e683a3e49967b237
749 <749@users.noreply.github.com>
2021-07-03 00:36:19 +0200

    feat(player-login): it is now possible for players to configure their controller via a separate URL

--- index.js
+++ index.js
@@ -2,9 +2,14 @@ const fs = require('fs')
 const https = require('https')
 const WebSocket = require('ws')
 const url = require('url')
+const path = require('path')
+const express = require('express')
 const moment = require('moment')
 const connectionRegistry = require('./src/connection/registry')
 const messageDispatcher = require('./src/message/dispatcher')
+
+// configuration
+const PORT = 10443
 
 connectionRegistry.attach(messageDispatcher)
 
@@ -17,6 +22,7 @@ messageDispatcher.handlers.VTTRegistrationMessage.push(function registerEndpoint
   newConnection.receiver = message.receiver
   newConnection.controllerId = message['controller-id']
   if(newConnection.receiver) {
+    newConnection.players = message.players || []
     console.log("[%d] announce all known controllers to new receiver", moment().valueOf())
     connectionRegistry.getControllerConnections()
         .forEach(conn => newConnection.send(JSON.stringify({
@@ -47,20 +53,41 @@ messageDispatcher.handlers.VTTConfigurationMessage.push(function configureEndpoi
       })
 })
 
+const app = express()
+
+app.use(express.static('../test'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.post('/api/players/register', (req, res) => {
+  res.end()
+  const data = JSON.stringify({
+    type: "keyboard-login",
+    'controller-id': req.body['controller-id'],
+    'player-id': req.body['player-id']
+  })
+  console.log('[%d] sending "%s" to all receivers', moment().valueOf(), data)
+  connectionRegistry.getReceiverConnections().forEach(conn => {
+    conn.send(data)
+  })
+})
+
+app.get('/api/players', (req, res) => {
+  res.json(connectionRegistry.getReceiverConnections().flatMap(conn => conn.players))
+})
+
+app.get('/configure', (req, res) => {
+  res.sendFile(path.join(__dirname, '..','test','configure.html'))
+})
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '..','test','keypad.html'))
+})
+
 const server = https.createServer({
   cert: fs.readFileSync('./config/certs/snakeoil.pem'),
   key: fs.readFileSync('./config/certs/snakeoil.key')
-}, function httpRequest(req, res) {
-  fs.readFile('../test/keypad.html', function (err, data) {
-    if (err) {
-      res.writeHead(404)
-      res.end(JSON.stringify(err))
-      return
-    }
-    res.writeHead(200, { 'Content-Type': 'text/html' })
-    res.end(data, 'utf-8')
-  })
-})
+}, app)
 
 const wss = new WebSocket.Server({ noServer: true })
 
@@ -95,5 +122,6 @@ server.on('upgrade', function upgrade(request, socket, head) {
   }
 })
 
-server.listen(10443)
-
+server.listen(PORT, () => {
+  console.log('[%d] Now listening on Port %d', moment().valueOf(), PORT)
+})
